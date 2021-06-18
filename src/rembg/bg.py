9ec47b639003a301d47351c284032264cdcfaf08
gustavo-rddi <83828278+gustavo-rddi@users.noreply.github.com>
2021-06-18 17:50:46 +0100

    changed input type to numpy array

--- src/rembg/bg.py
+++ src/rembg/bg.py
@@ -88,7 +88,8 @@ def remove(
     alpha_matting_base_size=1000,
 ):
     model = get_model(model_name)
-    img = Image.open(io.BytesIO(data)).convert("RGB")
+#     img = Image.open(io.BytesIO(data)).convert("RGB")
+    img = Image.fromarray(data).convert("RGB")
     mask = detect.predict(model, np.array(img)).convert("L")
 
     if alpha_matting:
